2f3e80d606109529c84dd59f132c50dca5d7a9be
Rennix5321 <aleksandrborovskih7985@gmail.com>
2026-03-01 14:38:34 +0300

    Задание 1.3

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
-from fastapi.responses import FileResponse
 
 app = FastAPI()
 
-@app.get("/")
-def get_html():
-    return FileResponse("index.html")
+@app.post("/calculate")
+def calculate(num1: int, num2: int):
+    return {"result": num1 + num2}
